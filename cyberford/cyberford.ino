7ac3bbf860c4c3e3477eee7ab7f1a54150d0e922
jaypacamarra <jpacamar97@gmail.com>
2022-08-16 13:10:31 -0700

    Implement light control
    - tested with logic analyzer
    - left turn signal works
    - right turn signal works
    - stop signal works
    - For now RPi control has dummy set command code

--- cyberford/cyberford.ino
+++ cyberford/cyberford.ino
@@ -3,9 +3,10 @@
 
 // Include Cyber Ford libraries
 #include <CyberFord.h>
-/*
-#include <CyberFord_RPiControl.h>
+
+//#include <CyberFord_RPiControl.h>
 #include <CyberFord_LightControl.h>
+/*
 #include <CyberFord_SteeringControl.h>
 #include <CyberFord_MotorControl.h>
 #include <CyberFord_Logging.h>
@@ -27,10 +28,10 @@ void setup() {
 
   // RPi control - Task creation
   xTaskCreate( TaskRPiControl, "RPiControl", 128, NULL, 3, NULL );
-  // Light control - Task creation
+  //Light control - Task creation
   xTaskCreate( TaskLightControl, "LightControl", 128, NULL, 1, NULL );
   // Steering control - Task creation
-  xTaskCreate( TaskSteeringControl, "SteeringControl", 128, NULL, 2, NULL);
+  xTaskCreate( TaskSteeringControl, "SteeringControl", 128, NULL, 2, NULL );
   // Motor control - Task creation
   xTaskCreate( TaskMotorControl, "MotorControl", 128, NULL, 2, NULL );
   // Logging - Task creation
@@ -42,21 +43,43 @@ void loop() {} // Not used, everything done in tasks
 
 // RPi control task definition
 void TaskRPiControl( void *pvParameters ) { 
-  for(;;);
+  for(;;) {
+    CyberFord.setCommand(commandStopSignal);
+    vTaskDelay(1);
+  }
 }
+
 // Light control task definition
 void TaskLightControl( void *pvParameters ) {
-  for(;;);
+  for(;;) {
+    if(CyberFord.getCommand() == commandLeftTurnSignal)
+      CyberFord.leftTurnSignal();
+    
+    else if(CyberFord.getCommand() == commandRightTurnSignal)
+      CyberFord.rightTurnSignal();
+    
+    else if(CyberFord.getCommand() == commandStopSignal)
+      CyberFord.stopSignal();
+  }
 }
+
 // Steering control task definition
 void TaskSteeringControl( void *pvParameters ) {
-  for(;;);
+  for(;;) {
+    vTaskDelay(500);
+  }
 }
+
 // Motor control task definition
 void TaskMotorControl( void *pvParameters ) {
-  for(;;);
+  for(;;) {
+    vTaskDelay(500);
+  }
 }
+
 // Logging task definition
 void TaskLogging( void *pvParameters ) {
-  for(;;);
+  for(;;) {
+    vTaskDelay(500);
+  }
 }
