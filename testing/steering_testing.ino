25f6e1e9ba8e95da67495d5b9bde5f5460e39089
jaypacamarra <jpacamar97@gmail.com>
2022-08-22 14:12:25 -0700

    add steering test in testing folder

---
+++ testing/steering_testing.ino
@@ -0,0 +1,32 @@
+#include <Servo.h>
+
+Servo myservo;
+
+void setup() {
+    myservo.attach(9);
+}
+
+void loop() {
+    int angle = 90;
+    int i = 0;
+
+    #define UP      (1)
+    #define DOWN    (0)
+    bool dir = UP;
+
+    for(;;) {
+        myservo.write(angle + i);
+
+        if(dir==UP)
+            i++;    
+        else if(dir==DOWN)
+            i--;
+        
+        if(i==20)
+            dir=DOWN;
+        if(i=-20)
+            dir=UP;
+
+        delay(500); 
+    }
+}
